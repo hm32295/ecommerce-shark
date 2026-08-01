df5fcaf2c5c52f5dfc3d0a56b9cc2ab16d5c05fb
Hadeer Mamdouh Ali Ahmed <h.01100229773@gmail.com>
2026-08-01 22:24:51 +0300

    Add forgot and reset password functionality

--- controller/auth/reset_password.js
+++ controller/auth/reset_password.js
@@ -1,6 +1,55 @@
-const usersCollection=require("../../models/users_model")
-
+const usersCollection=require("../../models/users_model");
+const bcrypt=require("bcrypt");
 const resetPassword=async(req,res)=>{
 
+    const{email,verificationCode,newPassword}=req.body;
+    if (!email ||!verificationCode ||!newPassword){
+        return res.status(400).json({
+            status_code:"400",
+            msg:"All Fields Is Required",
+            data:null
+        }); 
+    };
+    
+    const ckeckUser= await usersCollection.findOne({email});
+    if (!ckeckUser){
+        return res.status(404).json({
+            status_code: "404",
+            msg: "This Email Is Not Found!!!!!",
+            data: null
+        });
+    };
+    
+    if(ckeckUser.verificationCode !==verificationCode){
+        return res.status(400).json({
+        status_code:"400",
+        msg:"Invalid Code",
+        });
+};
+
+    if (ckeckUser.verificationCodeExpiration<new Date()){
+        return res.status(400).json({
+        status_code:"400",
+        msg:"your code Is Expired",
+        });
+};
+
+const hashPassword = await bcrypt.hash(newPassword, 6);
+
+await usersCollection.findOneAndUpdate(
+    {email},
+        { 
+            password:hashPassword,
+            $unset:
+            {verificationCode: 1,
+            verificationCodeExpiration: 1}
+        }
+        );
+    
+return res.status(200).json({
+    status_code:"200",
+    msg:"You Reset Your Password Successfully!!!!",
+    data:null
+});
 }
 module.exports=resetPassword;
--- routes/auth_router.js
+++ routes/auth_router.js
@@ -5,7 +5,10 @@ const register_controller=require("../controller/auth/register");
 const login_controller=require("../controller/auth/login");
 const emailVerification_controller=require("../controller/auth/verify_email");
 const resend_verificationCode_controller=require("../controller/auth/resend_verificationCode");
-const logOut_controller=require("../controller/auth/logOut")
+const logOut_controller=require("../controller/auth/logOut");
+const forgetPassword_controller=require("../controller/auth/forget_password.js");
+const reset_password=require("../controller/auth/reset_password");
+
 const router=express.Router();
 
 router.post("/register",upload.single("images"),register_controller);
@@ -13,5 +16,7 @@ router.post("/login",login_controller);
 router.post("/verify-email",emailVerification_controller);
 router.post("/resend-verification-code",resend_verificationCode_controller);
 router.post("/log-out",logOut_controller);
+router.post("/forget-password",forgetPassword_controller);
+router.post("/reset-password",reset_password)
 
 module.exports=router;

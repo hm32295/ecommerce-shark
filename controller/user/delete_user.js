7793107f959e2eaf34ca7cb2c0a7fd6335b8ca37
Hadeer Mamdouh Ali Ahmed <h.01100229773@gmail.com>
2026-08-02 18:08:22 +0300

    delete user

--- controller/user/delete_user.js
+++ controller/user/delete_user.js
@@ -1,7 +1,20 @@
 const usersCollection=require("../../models/users_model");
 const deleteUser=async(req,res)=>{
-    
+    const userId = req.params.id;
+    const deletedUser = await usersCollection.findByIdAndDelete(userId);
 
+    if (!deletedUser) {
+    return res.status(404).json({
+        status_code: "404",
+        msg: "User Not Found",
+        data: null
+    });
+};
+    return res.status(200).json({
+            status_code :"200",
+            msg: "The User Is Deleted Succesfully",
+            data:null
+        });
 };
 
 module.exports=deleteUser;
--- routes/users_router.js
+++ routes/users_router.js
@@ -5,11 +5,13 @@ const checkRole_middlware=require("../middlewares/block_user");
 
 const inActive_controller=require("../controller/user/delete_my_account");
 const activate_account_controller=require("../controller/user/activate_account");
+const delete_user=require("../controller/user/delete_user");
 
 const router = express.Router();
 
 router.patch("/inactive-account",isLoginMiddleware,inActive_controller);
-router.post("/activate-account",activate_account_controller)
+router.post("/activate-account",activate_account_controller);
+router.delete("/delete-user/:id",isLoginMiddleware,checkRole_middlware,delete_user);
 
 
 module.exports=router;

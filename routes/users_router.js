c98159dad3d46a789c9cf65b0ac485f2cbb3009b
hm32295 <almkhbz245@example.com>
2026-08-02 17:36:52 +0300

    update

--- routes/users_router.js
+++ routes/users_router.js
@@ -1,6 +1,7 @@
 const blockUser = require('../controller/user/block_user')
 const getAllUser = require('../controller/user/get_all_user')
 const getSingleUser = require('../controller/user/get_user_by_id')
+const unBlockUser = require('../controller/user/un_block_user')
 const check_role = require('../middlewares/check_role')
 const is_login_middleware = require('../middlewares/is_login')
 
@@ -9,5 +10,6 @@ const routerUsers = require('express').Router()
 
 routerUsers.get('/',is_login_middleware,check_role, getAllUser)
 routerUsers.get('/:userId', getSingleUser)
-routerUsers.post('/:userId',is_login_middleware,check_role, blockUser)
+routerUsers.post('/block/:userId',is_login_middleware,check_role,blockUser)
+routerUsers.post('/un-block/:userId',is_login_middleware,check_role, unBlockUser)
 module.exports = routerUsers

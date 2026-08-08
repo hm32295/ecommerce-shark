c54952f3b10be6bc9488ed8e3ee92fc1f600212c
hm32295 <almkhbz245@example.com>
2026-08-08 18:30:18 +0300

    update cors

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ app.use(
   cors({
     origin: true,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use('/api/v1/order', Router)
